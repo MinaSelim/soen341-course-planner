7c1e51cd154513c3e66c1afdf7a75fa240962cae
Mina <mina.selim2005@hotmail.com>
2019-01-26 12:21:53 -0500

    added comments and course code

--- Common/src/skynet/scheduler/common/ICourse.java
+++ Common/src/skynet/scheduler/common/ICourse.java
@@ -2,9 +2,10 @@ package skynet.scheduler.common;
 
 public interface ICourse 
 {
-	public ICourse[] getPrerequisites();
-	public String getCourseName();
-	public int getCreditUnits();
+	public ICourse[] getPrerequisites(); // Comp-248
+	public String getCourseName(); // Object-Oriented programmming II
+	public String getCourseCode(); // Comp-249
+	public int getCreditUnits(); //3.5
 	public SemesterSeasons[] getCourseAvailability();
 	public SemesterSeasons[] getEngineerAvailability();
 }
