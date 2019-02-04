77f3c1db128360abf139f55659066039b3e0ec2a
AjithThanam <ajith.thanam@gmail.com>
2019-02-03 23:31:28 -0500

    Added comments

--- backend/SampleServer/src/main/java/PracticeServer/SampleServer/SampleServerApplication.java
+++ backend/SampleServer/src/main/java/PracticeServer/SampleServer/SampleServerApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SampleServerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		//Call to run the Server itself
 		SpringApplication.run(SampleServerApplication.class, args);
 	}
 
--- backend/SampleServer/src/main/java/PracticeServer/SampleServer/dao/CourseDao.java
+++ backend/SampleServer/src/main/java/PracticeServer/SampleServer/dao/CourseDao.java
@@ -7,9 +7,17 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+    Based on the request from the controller this Dao class will execute the
+    corresponding method to access the data and return whatever information is required
+ */
 @Repository
 public class CourseDao {
 
+    /*
+        Creating a list of 3 course object with the properties provided bellow,
+        each of the objects respective CourseCode acts as its key within the list
+     */
     private static Map<String, Course> courses;
 
     static
@@ -24,21 +32,25 @@ public class CourseDao {
         };
     }
 
+    //Get method that returns all existing object
     public Collection<Course> getAllCourses()
     {
         return this.courses.values();
     }
 
+    //Get method that returns a course object based on the objects course code
     public Course getCourseByCode(String courseCode)
     {
         return this.courses.get(courseCode);
     }
 
+    //Delete method that deletes a course object based off its course code
     public void removeCourseByCode(String courseCode)
     {
         this.courses.remove(courseCode);
     }
 
+    //Update method which, given a course objects course code, you can change all of its other properties
     public void updateCourse (Course course)
     {
         Course c = courses.get(course.getCourseCode());
@@ -47,6 +59,7 @@ public class CourseDao {
         courses.put(course.getCourseCode(), course);
     }
 
+    //Insert method, that allows to add a new course object to the server
 	public void insertCourse(Course course)
 	{
 		this.courses.put(course.getCourseCode(), course);
